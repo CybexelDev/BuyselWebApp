5e84f9b8bff1cf8104c0b78134728093370d5a70
Mudhasir P <mudhasirp9@gmail.com>
2026-02-26 10:39:45 +0530

    about section full

--- BuyselFE/src/Pages/About/About.jsx
+++ BuyselFE/src/Pages/About/About.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import AboutHeader from '../../Layouts/About/AboutHeader/AboutHeader'
 import ComprehesiveSection from '../../Layouts/About/ComprehensiveSection/ComprehesiveSection'
+import AppPromoBanner from '../../Components/AppPromoBanner/AppPromoBanner'
+import Footer from '../../Components/Footer/Footer'
 
 function About() {
   return (
     <div>
         <AboutHeader/>
         <ComprehesiveSection/>
+        <AppPromoBanner/>
+        <Footer/>
     </div>
   )
 }
