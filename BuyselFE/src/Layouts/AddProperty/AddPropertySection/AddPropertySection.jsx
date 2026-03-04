573bf3bf03569b87cec9e075f66084ce4baf6346
Mudhasir P <mudhasirp9@gmail.com>
2026-03-04 14:41:33 +0530

    fixed add property

--- BuyselFE/src/Layouts/AddProperty/AddPropertySection/AddPropertySection.jsx
+++ BuyselFE/src/Layouts/AddProperty/AddPropertySection/AddPropertySection.jsx
@@ -30,14 +30,12 @@ function AddPropertySection() {
     priceNegotiable:"",
     availableDate:"",
 
-    schedule: ""
   });
 
   return (
     <div className="bg-[#FFFFFF] min-h-screen p-6">
 
       <div className=" mx-auto flex gap-2 sm:gap-4 md:gap-6 lg:gap-8">
-      <div className="max-w-[1400px] mx-auto flex gap-8">
 
         <SidebarProgress step={step} />
 
@@ -69,10 +67,13 @@ function AddPropertySection() {
             next={() => setStep(step + 1)}
            back={() => setStep(step - 1)}
            />
-              next={() => setStep(2)}
-            />
-          )}
+
+
         </div>
+
+
+
+
       </div>
 
     </div>
