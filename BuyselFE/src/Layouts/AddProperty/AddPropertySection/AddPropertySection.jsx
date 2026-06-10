c4ad570d10a3d54577bf8518abf0cf0d9590639a
Mudhasir P <mudhasirp9@gmail.com>
2026-06-10 11:23:59 +0530

    changes in tabs and validation

--- BuyselFE/src/Layouts/AddProperty/AddPropertySection/AddPropertySection.jsx
+++ BuyselFE/src/Layouts/AddProperty/AddPropertySection/AddPropertySection.jsx
@@ -9,6 +9,7 @@ import SuccessModal from "../../../Components/AddProperty/SuccessModal";
 import Payment from "../../../Components/AddProperty/Payment";
 import { useSelector } from "react-redux";
 import { motion } from "framer-motion";
+import { toast } from "sonner";
 import {
   getPropertyById,
   postProperty,
@@ -388,7 +389,12 @@ if (id) {
     res = await userPostProperty(payload);
   }
 }
+console.log("API RESPONSE:", res);
 
+if (!res?.status) {
+  toast.error(res.message);
+  return;
+}
 
     if (res) {
       setShowSuccess(true);
