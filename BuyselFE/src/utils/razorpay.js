10274e7c1e106b48e959eabac78da5ac6bb2b78b
CybexelDev <developerscbe.cybexel@gmail.com>
2026-06-11 16:50:39 +0530

    owner responsive  change 11/6/2025

--- BuyselFE/src/utils/razorpay.js
+++ BuyselFE/src/utils/razorpay.js
@@ -47,6 +47,7 @@
 // };
 
 
+import { toast } from "sonner";
 import api from "../Api/axiosInstance";
 
 export const openRazorpay = async ({
@@ -73,7 +74,7 @@ export const openRazorpay = async ({
   const isLoaded = await loadScript();
 
   if (!isLoaded) {
-    alert("Razorpay SDK Failed to load");
+    toast("Razorpay SDK Failed to load");
     return;
   }
 
@@ -92,9 +93,7 @@ export const openRazorpay = async ({
 
     // ✅ CHECK RESPONSE
     if (!order?.payment?.razorpay_order_id) {
-
-      alert("Order creation failed");
-
+      toast("Order creation failed");
       return;
     }
 
@@ -167,7 +166,7 @@ export const openRazorpay = async ({
             "VERIFY PAYMENT RESPONSE"
           );
 
-          alert("Payment Successful ✅");
+          toast("Payment Successful ✅");
 
           // ✅ CALLBACK
           onSuccess && onSuccess(verifyRes.data);
@@ -182,7 +181,7 @@ export const openRazorpay = async ({
             "VERIFY ERROR"
           );
 
-          alert("Payment Verification Failed ❌");
+          toast("Payment Verification Failed ❌");
         }
       },
 
@@ -212,7 +211,7 @@ export const openRazorpay = async ({
         "PAYMENT FAILED"
       );
 
-      alert(response.error.description);
+      toast(response.error.description);
     });
 
   } catch (error) {
@@ -222,6 +221,6 @@ export const openRazorpay = async ({
       "CREATE PAYMENT ERROR"
     );
 
-    alert("Something went wrong");
+    toast("Something went wrong");
   }
 };
