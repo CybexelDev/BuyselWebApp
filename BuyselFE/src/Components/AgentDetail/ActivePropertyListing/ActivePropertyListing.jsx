c669b5edde0ddd1ea2b4cd90651e6850a6a044fb
CybexelDev <developerscbe.cybexel@gmail.com>
2026-06-16 14:04:39 +0530

    owner responsive  change 11/6/2025

--- BuyselFE/src/Components/AgentDetail/ActivePropertyListing/ActivePropertyListing.jsx
+++ BuyselFE/src/Components/AgentDetail/ActivePropertyListing/ActivePropertyListing.jsx
@@ -338,8 +338,37 @@ if (res?.properties) {
             {currentProperties.length > 0 ? (
   currentProperties.map((property, index) => (
     <div key={index} className="min-w-[280px] lg:min-w-0">
-      <Propertycard property={property} />
-    </div>
+ <Propertycard
+          property={property}
+          click={() =>
+            property.is_wishlisted
+              ? removeWishlist(property.id)
+              : addWishlist(property.id)
+          }
+             wishlistIcon={
+                          property.is_wishlisted ? (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="15px"
+                              height="15px"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                fill="#e11a1a"
+                                d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53z"
+                              />
+                            </svg>
+                          ) : (
+                            <Heart
+                              size={13}
+                              fill="none"
+                              stroke="black"
+                              className="scale-100"
+                            />
+                          )
+                        }
+        />
+            </div>
   ))
 ) : (
   <div className="col-span-full flex flex-col items-center justify-center py-20 text-center">
