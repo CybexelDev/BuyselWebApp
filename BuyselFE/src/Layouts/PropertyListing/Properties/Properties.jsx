35a9c013aba3c5cc203eb1d9fa590f6ddcd39006
N-meapp <developersnme@gmail.com>
2026-06-17 10:47:12 +0530

    ashif update

--- BuyselFE/src/Layouts/PropertyListing/Properties/Properties.jsx
+++ BuyselFE/src/Layouts/PropertyListing/Properties/Properties.jsx
@@ -5,7 +5,7 @@ import { addToWishlist, removeToWishlist } from '../../../Api/userApi';
 import { Heart } from "lucide-react";
 import { toast } from 'sonner';
 import SkeletonCard from '../../../Components/SkeletonCard/SkeletonCard';
-import noimage from "../../../assets/images/propertDetail/noimage.png"
+
 function PropertiesSection({ propertiesData }) {
 
   const [itemsPerPage, setItemsPerPage] = useState(12);
@@ -15,13 +15,21 @@ function PropertiesSection({ propertiesData }) {
 
   console.log(properties, "propertiesData in properties section ???????????????");
 
+// useEffect(() => {
+
+//   if (propertiesData.length > 0) {
+//     setProperties(propertiesData || []);
+//     setLoading(false);
+//   }
+
+// }, [propertiesData]);
+
 useEffect(() => {
+  
+  setProperties(propertiesData || []);
 
-  if (propertiesData && propertiesData.length > 0) {
-    setProperties(propertiesData);
-    setLoading(false);
-  }
 
+  setLoading(false);
 }, [propertiesData]);
 
 
@@ -97,73 +105,35 @@ useEffect(() => {
       )
     );
   };
-const isFiltered = propertiesData?.length > 0 && properties?.length === 0;
+
   return (
     <div className='py-8 px-1 md:px-6 lg:px-8 mb-2 -mt-20'>
 
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 sm:gap-4">
-       
-        {loading ? (
-          Array.from({ length: 8 }).map((_, index) => (
+        {loading
+          ? Array.from({ length: 8 }).map((_, index) => (
             <SkeletonCard key={index} />
           ))
-        ) : currentProperties?.length > 0 ? (
-          currentProperties.map((property) => (
+          : currentProperties.map((property) => (
             <Propertycard
-              key={property.id}
               click={() =>
                 property.is_wishlisted
                   ? removeWishlist(property.id)
                   : addWishlist(property.id)
               }
-              wishlistIcon={
-                property.is_wishlisted ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="15px"
-                    height="15px"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      fill="#e11a1a"
-                      d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53z"
-                    />
-                  </svg>
-                ) : (
-                  <Heart
-                    size={13}
-                    fill="none"
-                    stroke="black"
-                    className="scale-100"
-                  />
-                )
-              }
+              wishlistIcon={property.is_wishlisted ? (
+                <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" viewBox="0 0 24 24">
+                  <path fill="#e11a1a" d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53z" />
+                </svg>
+              ) : (
+                <Heart size={13} fill="none" stroke="black" className="scale-100" />
+              )}
+              key={property.id}
               property={property}
               color="bg-[#fbfbfb]"
               shadow="shadow-[0px_4px_13.5px_0px_rgba(129,105,105,0.25)]"
             />
-          ))
-        ) : (
-          <div className="col-span-2 md:col-span-2 lg:col-span-3 xl:col-span-4 flex flex-col items-center justify-center py-20">
-            <img
-              src={noimage}
-              alt="No Properties"
-              className="w-40 mb-4 opacity-80"
-            />
-           <h3 className="text-xl font-semibold text-gray-700">
-  {isFiltered
-    ? "No Matching Properties Found"
-    : "No Properties Available"}
-</h3>
-
-<p className="text-gray-500 mt-2 text-center max-w-md">
-  {isFiltered
-    ? "We couldn't find any properties matching your search criteria. Try adjusting your filters."
-    : "There are currently no properties available in this category. Check back later for new listings."}
-</p>
-          </div>
-        )}
-
+          ))}
       </div>
 
 
