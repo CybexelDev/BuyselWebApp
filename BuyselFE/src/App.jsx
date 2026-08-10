a299d4b1b392deb4b6b7e9011343294076d14019
Mudhasir P <mudhasirp9@gmail.com>
2026-08-10 12:44:26 +0530

    first commit

--- BuyselFE/src/App.jsx
+++ BuyselFE/src/App.jsx
@@ -40,7 +40,7 @@ import Faqs from './Pages/Faqs/Faqs'
 import InvoicePage from './Pages/Invoice/Invoice'
 import AgentProtectedRoute from './Agent/Components/ProtectionRouteAgent/ProtectionRouteAgent'
 import UserProtectedRoute from './Components/ProtectionRoute/ProtectionRoute'
-import CommonProtectedRoute from './Components/CommonProtectionRoute/commonProtectedRoute'
+import CommonProtectedRoute from './Components/CommonProtectionRoute/CommonProtectedRoute'
 import Orders from './Agent/Pages/Orders/Orders'
 
 function App() {
