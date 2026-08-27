e2377e88110e0069a884eba020be9eb98d5ad914
CybexelDev <developerscbe.cybexel@gmail.com>
2026-08-27 14:52:18 +0530

    27/8/2026 buysel agent sidebar updation

--- BuyselFE/src/Agent/Components/ProtectionRouteAgent/ProtectionRouteAgent.jsx
+++ BuyselFE/src/Agent/Components/ProtectionRouteAgent/ProtectionRouteAgent.jsx
@@ -30,3 +30,40 @@ if (
 };
 
 export default AgentProtectedRoute;
+
+
+
+
+
+
+
+
+// import { useSelector } from "react-redux";
+// import { Navigate, useLocation } from "react-router-dom";
+
+// const AgentProtectedRoute = ({ children }) => {
+//   const location = useLocation();
+
+//   const agent = useSelector((state) => state.agent);
+
+//   if (!agent.isLoggedIn) {
+//     return <Navigate to="/loginandsignup" replace />;
+//   }
+
+//   const basicAgentRoutes = [
+//     "/agent/profile",
+//     "/agent/user-enquiry",
+//     "/agent/inbox",
+//   ];
+
+//   if (
+//     agent.agent_type === "basic" &&
+//     !basicAgentRoutes.includes(location.pathname)
+//   ) {
+//     return <Navigate to="/agent/profile" replace />;
+//   }
+
+//   return children;
+// };
+
+// export default AgentProtectedRoute;
