4880991d306f5ed9245ec22da74c6a8674785455
Mudhasir P <mudhasirp9@gmail.com>
2026-09-17 15:22:32 +0530

    th

--- BuyselFE/src/Components/ProtectionRoute/ProtectionRoute.jsx
+++ BuyselFE/src/Components/ProtectionRoute/ProtectionRoute.jsx
@@ -1,12 +1,21 @@
 import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { toast } from "sonner";
 
-const UserProtectedRoute = ({ children }) => {
-
+const UserProtectedRoute = ({ children, message }) => {
   const persistRoot = JSON.parse(
     localStorage.getItem("persist:root")
   );
 
   const user = JSON.parse(persistRoot?.user || "{}");
+
+  useEffect(() => {
+    if (!user?.isLoggedIn && message) {
+      toast.error(message, {
+        id: "login-required",
+      });
+    }
+  }, [user?.isLoggedIn, message]);
 
   if (!user?.isLoggedIn) {
     return <Navigate to="/loginandsignup" replace />;
