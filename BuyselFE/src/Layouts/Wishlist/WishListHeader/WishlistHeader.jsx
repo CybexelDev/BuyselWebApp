8c360891d15c14fbfa2eac9783135a6503043d82
CybexelDev <developerscbe.cybexel@gmail.com>
2026-09-18 17:33:42 +0530

    some edits on ui plans

--- BuyselFE/src/Layouts/Wishlist/WishListHeader/WishlistHeader.jsx
+++ BuyselFE/src/Layouts/Wishlist/WishListHeader/WishlistHeader.jsx
@@ -1,30 +1,45 @@
 
-import React, { useEffect, useState } from "react";
-import './wishlist.css'
-import logo from '../../../assets/images/logo/logo.png'
+import React from "react";
+import "./wishlist.css";
+import logo from "../../../assets/images/logo/logo.png";
 import Navbar from "../../../Components/Navbar/Navbar";
-import prop from '../../../assets/images/icons/prop.svg'
+import bgPromotionalBanner from "../../../assets/images/plan/bgPromotionalBanner.png";
 
 const WishlistHeader = () => {
+  return (
+    <div className="md:p-5 p-2 pb-0 relative">
+      <Navbar />
 
-    return (
-        <div className='md:p-5 p-2 pb-0 relative'>
-            <Navbar />
-            <div className="wishlist-cta-container">
-                <div class="wishlist-cta-logo-container ">
-                    <div className='flex items-center justify-center'>
-                        <img src={logo} alt="logo" className="wishlist-cta-logo w-[100px] " />
-                    </div>
+      <div className="wishlist-cta-container relative overflow-hidden">
 
-                </div>
-                <img
-                    src={prop}
-                    alt="Empty"
-                    className="md:w-[250px] w-[150px] md:h-[250px] h-[150px] md:mb-2 mb-7"
-                />
-            </div>
+        {/* Background Image */}
+        <div
+          className="
+            absolute inset-0 opacity-10
+            pointer-events-none
+            
+            bg-[length:100%_100%]
+            bg-[position:center_bottom]
+          "
+          style={{
+            backgroundImage: `url(${bgPromotionalBanner})`,
+          }}
+        />
+
+        {/* Logo */}
+        <div className="wishlist-cta-logo-container relative z-10">
+          <div className="flex items-center justify-center">
+            <img
+              src={logo}
+              alt="logo"
+              className="wishlist-cta-logo w-[100px]"
+            />
+          </div>
         </div>
-    )
-}
 
-export default WishlistHeader
+      </div>
+    </div>
+  );
+};
+
+export default WishlistHeader;
