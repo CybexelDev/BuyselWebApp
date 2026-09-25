d8c2a6bd44ca9027a20889496a4e0c21b4627b7d
N-meapp <developersnme@gmail.com>
2026-09-25 15:30:24 +0530

    ashi update

--- BuyselFE/src/Layouts/Home/Hero/Hero.jsx
+++ BuyselFE/src/Layouts/Home/Hero/Hero.jsx
@@ -50,13 +50,10 @@ const Hero = () => {
             <div className="flex flex-col justify-center h-full gap-4 text-center md:text-left"  >
               <p className="md:text-[35px] text-[28px] instrument-sans font-bold ">Start Your<br />Real-<span className="text-[#b8b8b8]">Estate</span><br />Journey Today!</p>
               <p className="md:text-[16px] text-[15px] text-[#000000] md:max-w-[400px] host-grotesk">Discover lands, homes, commercial buildings, hotels, malls and investment properties — all verified and listed by genuine owners and trusted agents.</p>
-
-            
               <div className="flex justify-center md:justify-start ">
                 <ButtonHead text={"Explore Properties"} />
               </div>
             </div>
-
           </div>
 
           <div className="relative w-full lg:w-[63%] h-[50vh] lg:h-[90vh] overflow-hidden transition-all duration-700"
@@ -86,8 +83,6 @@ const Hero = () => {
 
         </div>
       </div>
-
-
       <div className="flex md:gap-5 gap-2 md:pr-5 pr-2 absolute mb-[-70px] md:mb-5 left-8 bottom-[80px] z-10">
         <div className=''>
           <p className=' md:text-[24px] text-[18px] font-semibold instrument-sans'>10,00+</p>
