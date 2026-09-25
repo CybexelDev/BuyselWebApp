cd023c28df3c408d1ff9528dff294df414cf42e4
Mudhasir P <mudhasirp9@gmail.com>
2026-09-25 10:33:27 +0530

    one last final code

--- BuyselFE/src/Agent/Layouts/Plans/Agentplants.jsx
+++ BuyselFE/src/Agent/Layouts/Plans/Agentplants.jsx
@@ -230,10 +230,12 @@ if (result) {
 
     <div className="grid gap-8">
       {upgradePlans.map((agent) => {
-        const activePlan =
-          agent.plans.find(
-            (p) => p.plan_id === selectedPlan[agent.id]
-          ) || agent.plans[0];
+      const activePlan =
+  agent.plans?.find(
+    (p) => p.plan_id === selectedPlan[agent.id]
+  ) || agent.plans?.[0];
+
+if (!activePlan) return null;
 
         return (
           <PlanCard
